3b1c89e97d7a129379e4dc3c35e943127116674e
huangzh <hzh8311@mail.ustc.edu.cn>
2016-01-06 19:20:04 +0800

    first commit

---
+++ checkcode/views.py
@@ -0,0 +1,13 @@
+#-*-utf8-*-
+from django.shortcuts import render
+
+# Create your views here
+# process the checkcode function
+def checkcode(request,img=None):
+	'''
+	request:
+	img:the checkcode image
+	return a string "result" to render the blank to be filled in the website
+	'''	
+	result = None
+	return result
